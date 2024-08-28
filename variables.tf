69b4fc2e43b2674b487b9a725df381da0b17ac59
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-08-28 10:56:00 +0530

    added script related SG for all instance as module

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+
+}
+variable "environment" {
+
+}
+
+variable "sg_name" {
+  #default = mongodb
+}
+variable "common_tags" {
+  default = {}
+  type    = map(any)
+}
+variable "sg_tags" {
+  default = {}
+  type    = map(any)
+}
+variable "vpc_id" {
+
+}
+variable "sg_description" {
+  #default = ""
+  type = string
+}
+variable "sg_igress_rules" {
+  type    = list(any)
+  default = []
+}
